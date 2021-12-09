f51e19651e944dec7aa76cc8c899866f66f5d8fd
Hoang Son <hvsson102@gmail.com>
2021-12-09 14:45:52 +0700

    topic_1

---
+++ topic_1/topic_1.rb
@@ -0,0 +1,28 @@
+# Bài 1 
+# 1_a
+params = {
+	"supplier"=>{
+		"name"=>"Test lay params", 
+		"tax_number"=>"0110254854", 
+		"address"=>"Tokyo", 
+		"supplier_type"=>"personal", 
+		"bank_accounts"=>{
+			"0"=>{
+				"account"=>"847445", 
+				"branch_name"=>"VCB"
+			}, 
+			"1"=>{
+				"account"=>"985784", 
+				"branch_name"=>"ACB", 
+				"_destroy"=>"false"
+			}
+		}
+	}
+}
+
+puts params["supplier"]["bank_accounts"]["1"]["branch_name"]
+
+# 1_b
+
+params["type"] = "personal"
+puts params
