dc6715ede31e01ee229af44a4aad4260a6966e09
Hoang Son <hvsson102@gmail.com>
2021-12-13 16:31:40 +0700

    up tpoic 1

--- topic_1/topic_1.rb
+++ topic_1/topic_1.rb
@@ -19,10 +19,58 @@ params = {
 		}
 	}
 }
-
 puts params["supplier"]["bank_accounts"]["1"]["branch_name"]
 
 # 1_b
-
 params["type"] = "personal"
 puts params
+
+# Bài 2
+
+# 2_a
+a = [2,20,1,"/a","/c"]
+b = [1,"/c/a/","/b","/a",50]
+c = 1
+puts b.include?c
+
+# # 2_b
+a = [2,20,1,"/a","/c"]
+b = [1,"/c/a/","/b","/a",50]
+d = a & b
+puts d.inspect
+
+# 2_c
+a = [2,20,1,"/a","/c"]
+b = [1,"/c/a/","/b","/a",50]
+e = a - b
+puts e.inspect
+
+# Bài 3
+module Price
+  def self.min_ticket_need_sell
+    puts "You need sell at least 100 ticket to fly"
+  end
+end
+puts Price.min_ticket_need_sell
+
+class Plane 
+	include Price
+	def flight_attendant xxx
+		puts "In flight have #{xxx} attendant"
+	end
+
+	def self.max_fly_speed
+		puts "913 km/h"
+	end
+end
+
+# 3_a
+vietnam_airline = Plane.new.flight_attendant(10)
+puts vietnam_airline
+
+# 3_b
+vietjet = Plane.new.flight_attendant(20)
+puts vietjet
+
+# 3_c
+puts Plane.max_fly_speed
